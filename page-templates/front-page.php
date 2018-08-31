61756c6cf51a038d1b8e97dc004f713bda80f728
camille villard <hello@camillevillard.com>
2018-08-31 17:29:51 -0400

    test

--- page-templates/front-page.php
+++ page-templates/front-page.php
@@ -14,16 +14,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <div class="wrapper" id="frontpage-wrapper">
 
-	<!-- <div id="frontpage-content"> -->
-
-		<!-- <div class="row"> -->
-
-			<!-- <div class="col-md-12 content-area" id="primary"> -->
-
-				<!-- <main class="site-main" id="main" role="main"> -->
-
-						<?php get_template_part( 'views/frontpage-content' ); ?>
-
+		<?php get_template_part( 'views/frontpage-content' ); ?>
 
 </div><!-- Wrapper end -->
 
