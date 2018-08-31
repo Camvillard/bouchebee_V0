cb32ceb0cc08f07752b82dc853e00640eef72a46
camille villard <hello@camillevillard.com>
2018-08-31 17:01:18 -0400

    header navigation

---
+++ views/frontpage-content.php
@@ -0,0 +1,22 @@
+<div id="frontpage-intro">
+  <div class="frontpage-content">
+    <h1>en voir de toutes les couleurs</h1>
+    <p>bouche bee c'est une trop cool boutique</p>
+  </div>
+</div>
+
+<div id="frontpage-infos" >
+  <h3>un peu plus d'infos ici</h3>
+  <p>bouche bee c'est une trop cool boutique</p>
+  <h3>horaires</h3>
+<ul class="horaires">
+  <li>lundi: fermé, je me repose</li>
+  <li>mardi: 10h30 - 18h00</li>
+  <li>mercredi: 10h30 - 18h00</li>
+  <li>jeudi: 10h30 - 21h00</li>
+  <li>vendredi: 10h30 - 21h00</li>
+  <li>samedi: 10h00 - 17h00</li>
+  <li>dimanche: 10h00 - 17h00</li>
+</ul>
+
+</div>
