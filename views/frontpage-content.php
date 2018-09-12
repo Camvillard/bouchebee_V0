89bda4509f2b6fb5670dd6c3755f7aa946ec95e1
camille villard <hello@camillevillard.com>
2018-09-11 22:05:27 -0400

    frontpage columns & colors

--- views/frontpage-content.php
+++ views/frontpage-content.php
@@ -5,21 +5,28 @@
   </div>
 </div>
 
-<div id="frontpage-infos" >
-  <div class="frontpage-content">
-    <h3>un peu plus d'infos ici</h3>
-    <p>bouche bee c'est une trop cool boutique</p>
-    <h3>horaires</h3>
-  <ul class="horaires">
-    <li>lundi: fermé, je me repose</li>
-    <li>mardi: 10h30 - 18h00</li>
-    <li>mercredi: 10h30 - 18h00</li>
-    <li>jeudi: 10h30 - 21h00</li>
-    <li>vendredi: 10h30 - 21h00</li>
-    <li>samedi: 10h00 - 17h00</li>
-    <li>dimanche: 10h00 - 17h00</li>
-  </ul>
-  </div>
+<div class="container">
 
+  <div class="row">
+
+    <div class="col-md-4" id="frontpage-horaires" >
+      <h2>horaires</h2>
+      <ul class="horaires">
+        <li>lundi: fermé, je me repose</li>
+        <li>mardi: 10h30 - 18h00</li>
+        <li>mercredi: 10h30 - 18h00</li>
+        <li>jeudi: 10h30 - 21h00</li>
+        <li>vendredi: 10h30 - 21h00</li>
+        <li>samedi: 10h00 - 17h00</li>
+        <li>dimanche: 10h00 - 17h00</li>
+      </ul>
+    </div>
+
+    <div class="col-md-8 frontpage-content" id="frontpage-infos">
+      <h3>un peu plus d'infos ici</h3>
+      <p>bouche bee c'est une trop cool boutique</p>
+    </div>
+
+  </div>
 
 </div>
