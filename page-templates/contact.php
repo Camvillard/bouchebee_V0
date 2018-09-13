ff98dd1f2472d29620c9b66fca80198048575d27
camille villard <hello@camillevillard.com>
2018-09-12 20:07:39 -0400

    contact page

--- page-templates/contact.php
+++ page-templates/contact.php
@@ -14,11 +14,15 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <div class="wrapper" id="contact-page">
 
+	<div id="contact-back-home" class="btn-back">
+		<a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"> retour au site</a>
+	</div>
+
 	<div class="<?php echo esc_attr( $container ); ?>" id="content">
 
 		<div class="row">
 
-			<div class="col-md-8 content-area" id="primary">
+			<div class="col-md-12 content-area" id="primary">
 
 				<main class="site-main" id="main" role="main">
 
