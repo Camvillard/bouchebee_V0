97eac3f66c90e43ebfca175b2e85f2b955dd68ab
camille villard <hello@camillevillard.com>
2018-10-11 14:46:27 -0400

    mailchimp plugin basic style

--- page-templates/soon.php
+++ page-templates/soon.php
@@ -12,17 +12,18 @@ get_template_part( 'global-templates/header', 'empty' );
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
+<div id="btn-back-home">
+	<i class="fa fa-arrow-left"></i>
+	<a class="btn-back-white" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"> retour au site</a>
+</div>
+
 <div class="wrapper" id="comingsoon-page">
-
-
-	<div id="btn-back-home">
-		<i class="fa fa-arrow-left"></i>
-		<a class="btn-back-white" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"> retour au site</a>
-	</div>
 
 	<div class="<?php echo esc_attr( $container ); ?>" id="content">
 
 				<main class="site-main" id="main" role="main">
+
+					<h3 class="comingsoon-title">coming soon</h3>
 
 					<?php while ( have_posts() ) : the_post(); ?>
 
@@ -30,7 +31,16 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 					<?php endwhile; // end of the loop. ?>
 
+					<div id="newsletter-form">
+						<?php echo do_shortcode('[mc4wp_form id="76"]'); ?>
+					</div>
+
+
+
+					</div>
+
 				</main><!-- #main -->
+
 
 	</div><!-- Container end -->
 
